1da6bccc02cb03b03454dcb49a122b310f350876
胡涛 <hutao1@ganji.com>
2015-07-09 16:41:17 +0800

    －Modify right－

--- GJCUCapture/GJCUCapture/GJCUCapture/GJCUFlashModeView.h
+++ GJCUCapture/GJCUCapture/GJCUCapture/GJCUFlashModeView.h
@@ -3,7 +3,7 @@
 //  GJCoreUserInterface
 //
 //  Created by ZYVincent on 15-1-23.
-//  Copyright (c) 2015年 ganji. All rights reserved.
+//  Copyright (c) 2015年 ZYProSoft. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
